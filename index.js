3dbb6e960ccc5c1c87967a9eb090335badeb5b7e
Karim Makhloufi <karim@kazoo.agency>
2020-11-18 17:20:23 +0100

    Set the correct port from env variable

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World from Nodejs!");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
